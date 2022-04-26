5a12b5898afa81086dc503c8535eeb8b5aca4b98
Artem Poliakov <artem4250@gmail.com>
2022-04-27 02:44:27 +0300

    HeapNode destructor, ComplexBinomialHeap::empty, ComplexBinomialHeap::min

--- ComplexBinomialHeap.cpp
+++ ComplexBinomialHeap.cpp
@@ -2,4 +2,29 @@
 // Created by Artem on 26.04.2022.
 //
 
+#include <cassert>
 #include "ComplexBinomialHeap.h"
+
+ComplexBinomialHeap::HeapNode::~HeapNode()
+{
+    delete child;
+    delete sibling;
+}
+
+bool ComplexBinomialHeap::empty() const
+{
+    return head;
+}
+
+Complex ComplexBinomialHeap::min() const
+{
+    assert(!empty());
+    HeapNode* curr = head;
+    Complex res = head->key;
+    while(curr)
+    {
+        res = std::min(res, curr->key);
+        curr = curr->sibling;
+    }
+    return res;
+}
--- ComplexBinomialHeap.h
+++ ComplexBinomialHeap.h
@@ -5,7 +5,6 @@
 #ifndef COMPLEXBINOMIALHEAP_H
 #define COMPLEXBINOMIALHEAP_H
 
-#include <memory>
 #include "Complex.h"
 
 class ComplexBinomialHeap
@@ -13,17 +12,19 @@ class ComplexBinomialHeap
     struct HeapNode
     {
         Complex key;
-        std::shared_ptr<HeapNode> p; //parent
-        std::shared_ptr<HeapNode> child; //left child
-        std::shared_ptr<HeapNode> sibling; //right
+        HeapNode* p; //parent
+        HeapNode* child; //left child
+        HeapNode* sibling; //right sibling
         size_t degree;
         HeapNode() : key(0), p(nullptr), child(nullptr), sibling(nullptr), degree(0) {};
         explicit HeapNode(const Complex& key) : key(key), p(nullptr), child(nullptr), sibling(nullptr), degree(0) {};
+        ~HeapNode();
     };
-    std::shared_ptr<HeapNode> head;
+    HeapNode* head;
 public:
     ComplexBinomialHeap() : head(nullptr) {}; //створення порожньої біноміальної піраміди
-    //Complex min() const; //пошук мінімального ключа
+    bool empty() const;
+    Complex min() const; //пошук мінімального ключа
     //ComplexBinomialHeap merge(const ComplexBinomialHeap& first, const ComplexBinomialHeap& second); //злиття двох біноміальних пірамід
     //void insert(const Complex& k); //вставка вузла
     //Complex extractMin(); //вилучення мінімального вузла
