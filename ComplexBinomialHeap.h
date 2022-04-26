b77663d99334b4be8baf632646c7b9153a8b9dff
Artem Poliakov <artem4250@gmail.com>
2022-04-27 00:33:01 +0300

    HeapNode constructors, ComplexBinomialHeap method signatures

--- ComplexBinomialHeap.h
+++ ComplexBinomialHeap.h
@@ -17,7 +17,18 @@ class ComplexBinomialHeap
         std::shared_ptr<HeapNode> child; //left child
         std::shared_ptr<HeapNode> sibling; //right
         size_t degree;
+        HeapNode() : key(0), p(nullptr), child(nullptr), sibling(nullptr), degree(0) {};
+        explicit HeapNode(const Complex& key) : key(key), p(nullptr), child(nullptr), sibling(nullptr), degree(0) {};
     };
+    std::shared_ptr<HeapNode> head;
+public:
+    ComplexBinomialHeap() : head(nullptr) {}; //створення порожньої біноміальної піраміди
+    //Complex min() const; //пошук мінімального ключа
+    //ComplexBinomialHeap merge(const ComplexBinomialHeap& first, const ComplexBinomialHeap& second); //злиття двох біноміальних пірамід
+    //void insert(const Complex& k); //вставка вузла
+    //Complex extractMin(); //вилучення мінімального вузла
+    //bool decrease(const Complex& old, const Complex& current); //зменшення ключа
+    //bool remove(const Complex& k); //видалення ключа
 };
 
 
