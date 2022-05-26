2d7727e44ff01d1a3f6713bb21185ebf4e47e02e
Artem Poliakov <artem4250@gmail.com>
2022-05-26 16:58:44 +0300

    major update, class Interval, ComplexIntervalTree::insert

--- ComplexIntervalTree.cpp
+++ ComplexIntervalTree.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "ComplexIntervalTree.h"
+
+Complex ComplexIntervalTree::TreeNode::key() const
+{
+    return data.low;
+}
+
+ComplexIntervalTree::ComplexIntervalTree()
+{
+    nil = new TreeNode(BLACK);
+    head = nil;
+}
+
+void ComplexIntervalTree::insert(const Interval& toInsert)
+{
+    auto z = new TreeNode(toInsert);
+    auto y = nil;
+    auto x = head;
+    while(x != nil)
+    {
+        y = x;
+        if(z->key() < x->key()) x = x->left;
+        else x = x->right;
+    }
+    z->p = y;
+    if(y == nil) head = z;
+    else if(x->key() < y->key()) y->left = z;
+    else y->right = z;
+    z->left = nil;
+    z->right = nil;
+    z->color = RED;
+    insertFix(z);
+}
--- ComplexIntervalTree.h
+++ ComplexIntervalTree.h
@@ -7,9 +7,44 @@
 
 #include "Complex.h"
 
+class Interval
+{
+public:
+    Complex low;
+    Complex high;
+
+    Interval(Complex low, Complex high) : low(low), high(high) {};
+};
+
 class ComplexIntervalTree
 {
+    enum Color
+    {
+        BLACK, RED
+    };
+    class TreeNode
+    {
+    public:
+        Interval data;
+        Complex max;
+        Color color;
+        TreeNode* p; //parent
+        TreeNode *left, *right;
 
+        explicit TreeNode(Interval interval) : data(interval), max(0), color(RED), p(nullptr), left(nullptr), right(nullptr) {};
+        explicit TreeNode(Color color) : data(Complex(0), Complex(0)), max(0), color(color), p(nullptr), left(nullptr), right(nullptr) {};
+        Complex key() const;
+    };
+
+    TreeNode* head;
+    TreeNode* nil;
+
+    void insertFix(TreeNode* z);
+public:
+    ComplexIntervalTree();
+    void insert(const Interval& toInsert);
+    void remove(const Interval& toRemove);
+    Interval searchIntersect(const Interval& toSearch) const;
 };
 
 #endif
